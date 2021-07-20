337ccd0a2ed793ddb5764c7b6849882cf5bf0161
Raphaella Robles <raphaella.robles@gmail.com>
2021-07-20 07:21:13 +0000

    Created more joined queries using GROUP BY and HAVING clauses

---
+++ 2_queries_joins/3_students_total_assignment_submissions.sql
@@ -0,0 +1,6 @@
+-- Find total of assignments submissions for all enrolled students using count function
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name;
